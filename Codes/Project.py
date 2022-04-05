1decd3749e82d27f0d47786ee9dba22636772645
enesbjk803 <enesbjk08@hotmail.com>
2022-04-05 23:51:12 +0300

    ush

--- Codes/Project.py
+++ Codes/Project.py
@@ -4,10 +4,10 @@ app = Flask(__name__)
 
 name = "Kerem Enes Ersoy"
 name_2 = "Ipek Cidik"
-
+x = 4
 @app.route("/")
 def index():
-    return render_template("indexflask.html", name1 = name , name_2 = name_2)
+    return render_template("indexflask.html", name1 = name , name_2 = name_2 , sayi = x)
 
 if __name__ == "__main__":
     app.run(debug=True)
