188768f658eecaf09bad3a2589644db019353f45
enesbjk803 <enesbjk08@hotmail.com>
2022-04-05 16:09:07 +0300

    push

--- Codes/Project.py
+++ Codes/Project.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template,flash
+from flask import Flask ,render_template,flash,url_for,redirect,session,logging,request
 from flask_mysqldb import MySQL
 
 app = Flask(__name__)
@@ -6,6 +6,6 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
-    
+
 if __name__ == "__main__":
     app.run(debug=True)
