2dc78a92a27d732a739a6423d89b75bcb693121b
enesbjk803 <enesbjk08@hotmail.com>
2022-05-03 18:35:19 +0300

    push

--- Codes/Project.py
+++ Codes/Project.py
@@ -1,4 +1,10 @@
 from flask import Flask ,render_template,flash,url_for,redirect,session,logging,request
+from flask_mysqldb import MySQL
+from wtforms import Form,StringField,TextAreaField,PasswordField,validators
+from passlib.hash import sha256_crypt
+import email_validator
+from functools import wraps
+
 
 app = Flask(__name__)
 app.secret_key = "E-MANAV"
