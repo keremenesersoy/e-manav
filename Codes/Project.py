6a3f7de53274228153eeb59c95e5c6cf17b3e110
enesbjk803 <enesbjk08@hotmail.com>
2022-05-04 00:02:30 +0300

    push

--- Codes/Project.py
+++ Codes/Project.py
@@ -1,4 +1,10 @@
+from tkinter.tix import Form
 from flask import Flask ,render_template,flash,url_for,redirect,session,logging,request
+
+#Register Form
+class RegisterForm(Form):
+    pass
+
 
 app = Flask(__name__)
 app.secret_key = "E-MANAV"
@@ -6,9 +12,14 @@ app.secret_key = "E-MANAV"
 name = "Kerem Enes Ersoys"
 name_2 = "Ipek Cidik"
 x = 4
+
 @app.route("/")
 def index():
     return render_template("indexflask.html")
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
 
 
 
