f711e109fc20a7de42db525a4066d949db733a43
ipekcdk <ipekcidik@gmail.com>
2022-05-04 01:30:59 +0300

    push

--- Codes/Project.py
+++ Codes/Project.py
@@ -1,23 +1,22 @@
-from crypt import methods
-from tokenize import String
-from flask import Flask ,render_template,flash,url_for,redirect,session,logging,request
+import email_validator
+from flask import Flask, render_template, flash, url_for, redirect, session, logging, request
 from flask_mysqldb import MySQL
-from wtforms import Form,StringField,TextAreaField,PasswordField,validators
+from wtforms import Form, StringField, TextAreaField, PasswordField, validators
 from passlib.hash import sha256_crypt
-from wtforms_components import PhoneNumberField
 
 
-#Register Form
+# Register Form
 class RegisterForm(Form):
-    Name = StringField("İsim :",validators=[validators.Length(min=2 , max=40)])
-    SurName = StringField("Soyisim :",validators=[validators.Length(min=2 , max=40)])
-    Password = PasswordField("Parola :",validators=[
-        validators.DataRequired(message="Lütfen Parola Belirleyin") , 
-        validators.EqualTo(fieldname = "Confirm" , message="Parola Uyuşmuyor")
+    Name = StringField("İsim :", validators=[validators.Length(min=2, max=40)])
+    SurName = StringField("Soyisim :", validators=[
+                          validators.Length(min=2, max=40)])
+    Password = PasswordField("Parola :", validators=[
+        validators.DataRequired(message="Lütfen Parola Belirleyin"),
+        validators.EqualTo(fieldname="Confirm", message="Parola Uyuşmuyor")
     ])
     Confirm = PasswordField("Parola Doğrula :")
-    Email = StringField("Email :",validators=[validators.Email(message="Lütfen geçerli bir e-mail adresi giriniz")])
-    
+    Email = StringField("Email :", validators=[validators.Email(
+        message="Lütfen geçerli bir e-mail adresi giriniz")])
 
 
 app = Flask(__name__)
@@ -27,11 +26,13 @@ name = "Kerem Enes Ersoys"
 name_2 = "Ipek Cidik"
 x = 4
 
+
 @app.route("/")
 def index():
     return render_template("indexflask.html")
 
-@app.route("/register" , methods = ["GET","POST"])
+
+@app.route("/register", methods=["GET", "POST"])
 def register():
 
     form = RegisterForm(request.form)
@@ -45,9 +46,7 @@ def register():
 
         #cursor = mysql.connection.cursor
 
-
     return render_template("register.html")
-
 
 
 if __name__ == "__main__":
