92a87c9866e5b559bf65daaa02ec48f96946f39f
enesbjk803 <enesbjk08@hotmail.com>
2022-05-04 16:43:11 +0300

    push

--- Codes/Project.py
+++ Codes/Project.py
@@ -1,17 +1,9 @@
-<<<<<<< HEAD
-from flask import Flask ,render_template,flash,url_for,redirect,session,logging,request
-=======
 import email_validator
 from flask import Flask, render_template, flash, url_for, redirect, session, logging, request
->>>>>>> f711e109fc20a7de42db525a4066d949db733a43
 from flask_mysqldb import MySQL
 from wtforms import Form, StringField, TextAreaField, PasswordField, validators
 from passlib.hash import sha256_crypt
-<<<<<<< HEAD
-import email_validator
 
-=======
->>>>>>> f711e109fc20a7de42db525a4066d949db733a43
 
 
 # Register Form
@@ -54,15 +46,15 @@ def register():
         Email = form.Email.data
 
         #cursor = mysql.connection.cursor
-<<<<<<< HEAD
         redirect(url_for("/"))
     else:
         return render_template("register.html",form = form)
-=======
+
 
     return render_template("register.html")
->>>>>>> f711e109fc20a7de42db525a4066d949db733a43
 
-
+app.route("/login")
+def login():
+    return render_template("login.html")
 if __name__ == "__main__":
     app.run(debug=True)
